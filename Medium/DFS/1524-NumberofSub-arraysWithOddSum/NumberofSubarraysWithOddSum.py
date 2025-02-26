721138792c76eb488b732174278e8762a8cbb2e2
Gustavo Moreira <gustavoluisperesmoreira@gmail.com>
2025-02-26 19:22:55 +0000

    adding but needs fix

--- Medium/DFS/1524-NumberofSub-arraysWithOddSum/NumberofSubarraysWithOddSum.py
+++ Medium/DFS/1524-NumberofSub-arraysWithOddSum/NumberofSubarraysWithOddSum.py
@@ -1,4 +1,3 @@
-
 from typing import List
 
 class Solution:
@@ -8,13 +7,29 @@ class Solution:
             prefixSum += i
             oddCount += prefixSum % 2
         oddCount += (len(arr) - oddCount) * oddCount
-        print(oddCount, oddCount % mod, mod)
         return oddCount % mod
     
         
-
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        trusted = [0] * (n+1)
+        for (a,b) in trust:
+            trusted[a] -= 1
+            trusted[b] += 1
+        
+        for i in range(1, len(trusted)):
+            if trusted[i] == n-1:
+                return i
+        return -1
+    
+    def maxAbsoluteSum(self, nums: List[int]) -> int:
+        maxNum = 0
+        for i in range(len(nums)):
+            for y in range(i, len(nums)):
+                subarrays = abs(sum(nums[i: y + 1]))
+                maxNum = max(maxNum, subarrays)
+        return (maxNum)
+      
 
 result = Solution()
-result.numOfSubarrays(arr = [1,3,5])
-# result.numOfSubarrays(arr=[1,2,3,4,5,6,7])
-# result.numOfSubarrays(arr=[2,4,6])
+result.maxAbsoluteSum(nums = [1,-3,2,3,-4])
+result.maxAbsoluteSum(nums = [2,-5,1,-4,3,-2])

3fdc12d251b3b290d9b66f42a3975a7bdfdf9e90
Gustavo Moreira <gustavoluisperesmoreira@gmail.com>
2025-09-10 14:04:41 +0100

    adding changes

--- top150/top150.py
+++ top150/top150.py
@@ -1,4 +1,30 @@
 from typing import List
+import random
+import heapq
+class RandomizedSet:
+    def __init__(self):
+        self.hmap = {}
+        self.list = []
+   
+    
+    def insert(self, val: int) -> bool:
+        if val in self.hmap:
+            return False
+        self.hmap[val] = len(self.list)
+        self.list.append(val)
+       
+    def remove(self, val: int) -> bool:
+        if val in self.hmap:
+            last_element, idx = self.list[-1], self.hmap[val]
+            self.list[idx], self.hmap[last_element] = last_element, idx
+            self.list.pop()
+            del self.hmap[val]            
+            return True
+        return False
+       
+    
+    def getRandom(self) -> int:
+        return random.choice(self.list)
 class Solution:
     def removeDuplicatesEasy(self, nums: List[int]) -> int:
         insertIndex = 1
@@ -97,12 +123,69 @@ class Solution:
         if r2 - r1 == 1:
             return True
         return False
-          
-        
-        
-        
-        
-
+    
+    
+    def jump(self, nums: List[int]) -> int:
+        l = r = 0
+        # ans = 0
+        # while r < len(nums) - 1:
+        #     s = max(nums[l:r + 1])
+        #     l = r + 1
+        #     r = s
+        #     ans += 1
+            
+        # return ans
+            
+    def lengthOfLastWord(self, s: str) -> int:
+        l, r = 0, len(s)
+        while r > 0:
+            r -= 1
+            if s[r] != " ":
+                l += 1
+            elif l > 0:
+                return l
+        return l
+    
+    #Day 3
+    
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        accumulator = 1
+        prefixIndex = len(nums) - 2
+        ans = [1] * len(nums)
+        for i in reversed(range(1, len(nums))):
+            accumulator *= nums[i]
+            ans[prefixIndex] = accumulator
+            prefixIndex -= 1
+        sufixIndex = 1
+        accumulator = 1
+        for i in range(len(nums) - 1):
+            accumulator *= nums[i]
+            ans[sufixIndex] *= accumulator
+            sufixIndex += 1
+        return ans
+    
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        prev_cos = 0
+        for i in range(len(gas)):
+            x = cost[i] % len(gas)
+            prev_cos = prev_cos - i + gas[x]
+            print(prev_cos - x, prev_cos)
+            
+    def hihestProduct(self, p: List[int]) -> int:
+        if len(p) < 3:
+            return 0
+        largest = heapq.nlargest(3, p)
+        smallest = heapq.nsmallest(2, p)
+        ans = max(largest[0] * largest[1] * largest[2], smallest[0] * smallest[1] * largest[0])
+        return ans
+    
+            
+            
 result = Solution()
+
+result.canCompleteCircuit(gas = [1,2,3,4,5], cost = [3,4,5,1,2])
+#result.canCompleteCircuit(gas = [2,3,4], cost = [3,4,3])
+
+
 
 

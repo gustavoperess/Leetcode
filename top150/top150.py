19ca22644fea56c74fcfe4f2805be43241a88395
Gustavo Moreira <gustavoluisperesmoreira@gmail.com>
2025-09-11 14:41:00 +0100

    Day 4 of 150 questions

--- top150/top150.py
+++ top150/top150.py
@@ -179,12 +179,136 @@ class Solution:
         ans = max(largest[0] * largest[1] * largest[2], smallest[0] * smallest[1] * largest[0])
         return ans
     
+    
+                
+        
+    #Day 4
+    def sortVowels(self, s: str) -> str:
+        # vowelsLocation = []
+        # vowels = "aeiouAEIOU"
+        # answer = ["0"] * len(s) 
+        # for i in range(len(s)):
+        #     if s[i] in vowels:
+        #         vowelsLocation.append((s[i], ord(s[i])))
+        #     else:
+        #         answer[i] = s[i]
+        # so  = sorted(vowelsLocation, key=lambda x: x[1])
+        # k = 0
+        # for i in range(len(s)):
+        #     if s[i] in vowels:
+        #         answer[i] = so[k][0]
+        #         k += 1
+        # return "".join(answer)
+        vowels = set("AEIOUaeiou")
+        vowelsLocation = [ch for ch in s if ch in vowels]
+        vowelsLocation.sort()
+        ans = []
+        k = 0
+        for ch in s:
+            if ch in vowels:    
+                ans.append(vowelsLocation[k])
+                k += 1
+            else:
+                ans.append(ch)
+        return "".join(ans)
+    
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        # if not strs:
+        #     return ""
+        # if len(strs) == 1:
+        #     return strs[0]
+        
+        # fw = strs[0]
+        # size = float("inf")
+        
+        # for i in range(1, len(strs)):
+        #     l = 0
+        #     while l < len(fw) and l < len(strs[i]) and strs[i][l] == fw[l]:
+        #             l += 1
+        #     size = min(size, l)
+        
+        # return fw[:size]
+        if not strs:
+            return ""
+        if len(strs) == 1:
+            return strs[0]
+        
+        ans = ""
+        strs=sorted(strs)
+        first = strs[0]
+        last = strs[-1] 
+        for i in range(min(len(first), len(last))):
+            if first[i] != last[i]:                
+                return ans
+            ans += first[i]
+        return ans
+    
+    def candy(self, ratings: List[int]) -> int:
+        childers = [1] *len(ratings)
+        for i in range(len(ratings)):
+            if ratings[i] > ratings[i-1]:
+                childers[i] = childers[i-1] + 1
+        
+        for i in reversed(range(len(ratings) - 2)):
+            if ratings[i] > ratings[i + 1]:
+                childers[i] = max(childers[i], childers[i + 1] + 1)
+        
+        return sum(childers)
+    
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l, r = 0, len(numbers) - 1
+        while l < r:
+            s = numbers[l] + numbers[r]
+            if s == target:
+                print(l + 1, r)
+                return [l + 1, r + 1]
+            if s < target:
+                l += 1
+            else:
+                r -= 1
+    
+    def isPalindrome(self, s: str) -> bool:  
+        l, r = 0, len(s) - 1
+        while l < r:
+            while l < r and not s[l].isalnum():
+                l += 1
+            while l < r and not s[r].isalnum():
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l += 1
+            r -= 1
+        return True
+    
+    def maxArea(self, height: List[int]) -> int:
+        l, r = 0, len(height) - 1 
+        min_h = float("inf")
+        ans = 0
+        while l < r:
+            min_h = min(height[l], height[r])
+            ans = max(ans , min_h * (r - l))
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1 
             
-            
-result = Solution()
+        return ans
+    
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        l, curr_sum  = 0, 0
+        ans = float("inf")
+        for r in range(len(nums)):
+            curr_sum += nums[r]
+            while curr_sum >= target:
+                ans = min(ans, (r -l) + 1)
+                curr_sum -= nums[l]
+                l += 1
+        return ans
 
-result.canCompleteCircuit(gas = [1,2,3,4,5], cost = [3,4,5,1,2])
-#result.canCompleteCircuit(gas = [2,3,4], cost = [3,4,3])
+        
+result = Solution()
+result.minSubArrayLen(target = 7, nums = [2,3,1,2,4,3])
+
 
 
 
